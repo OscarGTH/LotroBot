eac8431a0c5e960e21b68c82b8400d6785427655
Oskari Niskanen <oskari.niskanen97@gmail.com>
2022-04-29 01:16:08 +0200

    Added detection of monster being slain

--- src/run_bot.py
+++ src/run_bot.py
@@ -3,12 +3,15 @@ import pydirectinput as pd
 import pygetwindow as gw
 from logzero import logger
 import time
+import math
 from config_parser import get_configuration
 
 # Tuple to hold game region coordinates
 GAME_REGION = ()
 MINIMAP_REGION = (1600, 0, 290, 300)
-MINIMAP_CENTER = (1738, 153)
+# Point(x=1165, y=193)
+# Point(x=1561, y=192)
+COMBAT_LOG_REGION = (1165, 0, 396, 192)
 # Player moves around 14 pixels in ~1 second.
 RUN_SPEED = 14
 
@@ -55,10 +58,11 @@ class Bot:
         # If window was activated, find monster.
         if window_active:
             self.run_mob_killer()
+            
 
     def run_mob_killer(self):
         # TODO: Set better way to limit monster kill limit
-        for _ in range(2):
+        for _ in range(5):
             coords = self.find_monster()
             if coords:
                 # Get time to run to the target
@@ -72,8 +76,8 @@ class Bot:
     def calc_dist_to_monster(self, coords):
         """ Calculates the distance between the player and the monster. """
 
-        x_diff = abs(coords[0] - MINIMAP_CENTER[0])
-        y_diff = abs(coords[1] - MINIMAP_CENTER[1])
+        x_diff = abs(coords[0] - pg.center(MINIMAP_REGION)[0])
+        y_diff = abs(coords[1] - pg.center(MINIMAP_REGION)[1])
         # Selecting maximum difference of the two
         if x_diff >= y_diff:
             # Calculating time to run to the target
@@ -89,10 +93,15 @@ class Bot:
         # Run towards target
         pg.mouseDown(button='left')
         pg.mouseDown(button='right')
-        time.sleep(distance)
+        for i in range(math.floor(distance)):
+            pd.press("Space")
+            time.sleep(1)
         pg.mouseUp(button='left')
         pg.mouseUp(button='right')
 
+    #####
+    # IMAGE DETECTION METHODS
+    #####
 
     def find_monster(self):
         """ Finds a monster from the minimap and turns towards it. """
@@ -117,13 +126,59 @@ class Bot:
             return None
 
 
+    def is_mob_defeated(self):
+        """ Detects if mob was defeated by the player. """
+
+        mob_defeated_text = pg.locateOnScreen('images/monster-defeated.png', confidence=0.9, region=COMBAT_LOG_REGION)
+        if mob_defeated_text:
+            logger.info("Defeated text detected in combat log.")
+            return True
+        else:
+            return False
+
+    #####
+    # COMBAT METHODS
+    #####
+
     def attack_target(self):
         """ Attacks targeted enemy. """
-        # BIG TODO: Attack until enemy has died instead of attacking static amount of times.
-        max_attacks = 8
-        for _ in range(max_attacks):
+
+        # TODO: FIX THIS MESS
+        # Making sure player is facing enemy.
+        # Moving mouse to center of the screen.
+        pg.moveTo(pg.center(GAME_REGION)[0] + 100, pg.center(GAME_REGION)[1] + 100)
+        time.sleep(0.5)
+        # Turning camera to the monster.
+        pg.mouseDown(button='right')
+        pg.click()
+        time.sleep(0.5)
+        pg.mouseUp(button='right')
+
+        attack_count = 0
+        while(True):
             pd.press('1')
+            self.click_right_mouse()
             time.sleep(1)
+            if self.is_mob_defeated() and attack_count > 3:
+                break
+            else:
+                attack_count += 1
+
+    def use_skill(self, key):
+        # Uses a skill in-game by pressing the given key
+        pd.press(key)
+
+    #####
+    # HELPER METHODS
+    #####
+
+    def take_screenshot(self):
+        # Takes a full screen screenshot and saves it.
+        pg.screenshot('images/fullscreen_sc.png', region=COMBAT_LOG_REGION)
+
+    def click_right_mouse(self):
+        pg.mouseDown(button='right')
+        pg.mouseUp(button='right')
 
 if __name__ == "__main__":
     # Creating instance of the bot
